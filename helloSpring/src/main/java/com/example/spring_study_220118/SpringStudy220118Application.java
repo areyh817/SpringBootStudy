f830edaf6cefd38481d3b5b787eb38e5739d10ae
조혜라 <s2037@e-mirim.hs.kr>
2022-01-19 00:34:26 +0900

    [220119] hello spring ! :leaves:

---
+++ helloSpring/src/main/java/com/example/spring_study_220118/SpringStudy220118Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_study_220118;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudy220118Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStudy220118Application.class, args);
+    }
+
+}
---
+++ helloSpring/src/test/java/com/example/spring_study_220118/SpringStudy220118ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_study_220118;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudy220118ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
